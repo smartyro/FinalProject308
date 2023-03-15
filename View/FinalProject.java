53d13022ce3aed7db4b3d2bc2c1faf2e2a2f1bf3
Jared-Swislow <jayswis2002@gmail.com>
2023-03-14 17:42:23 -0700

    Made a window

--- View/FinalProject.java
+++ View/FinalProject.java
@@ -1,1 +1,17 @@
+package View;
 
+import javax.swing.*;
+import java.awt.*;
+
+public class FinalProject extends JFrame {
+	public static void main(String[] args) {
+		FinalProject f = new FinalProject();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setSize(new Dimension(600, 400));
+		f.setVisible(true);
+	}
+	
+	FinalProject() {
+	
+	}
+}
