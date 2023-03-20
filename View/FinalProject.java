52d3fdc6f18e1a70ca07a757a1c931dbab07c846
keilamohan <60747314+keilamohan@users.noreply.github.com>
2023-03-20 16:41:06 -0700

    main edited a bit

--- View/FinalProject.java
+++ View/FinalProject.java
@@ -1,9 +1,10 @@
 package View;
-
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.*;
 import java.awt.*;
 
-public class FinalProject extends JFrame {
+public class FinalProject extends JFrame implements ActionListener{
 	public static void main(String[] args) {
 		FinalProject f = new FinalProject();
 		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -11,9 +12,35 @@ public class FinalProject extends JFrame {
 		f.setVisible(true);
 	}
 	
-	FinalProject() {
-	
-	}
-}
+    public FinalProject() {
+
+        JPanel northPanel = new JPanel();
+        northPanel.setBackground(Color.WHITE);
+        JButton file = new JButton("File");
+        JButton help = new JButton("Help");
+
+        file.addActionListener(this);
+        help.addActionListener(this);
+
+
+        northPanel.add(file);
+        northPanel.add(help);
+
+        BorderLayout layout = new BorderLayout();
+        setLayout(layout);
+        add(northPanel, BorderLayout.NORTH);
+
+    }
+
+	/**
+     * Implemented Method from ActionListener class that allows the program to
+     * acces the button that has been clicked to invoke an action.
+     * 
+     * @param e
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String actionCommand = e.getActionCommand();
+    }
 
 
