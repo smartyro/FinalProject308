838a4b614fa0ec4c52013d37ea47dd38c0776038
keilamohan <60747314+keilamohan@users.noreply.github.com>
2023-03-20 16:37:00 -0700

    Set up the BlackBoard class for JPanel and the main for the project

--- View/BlackBoard.java
+++ View/BlackBoard.java
@@ -1,8 +1,7 @@
 package View;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 /**
  * 
  * Final Project 308.
@@ -10,38 +9,15 @@ import java.awt.event.ActionEvent;
  * @author 
  */
 
-public class BlackBoard extends JFrame implements ActionListener{
+public class BlackBoard extends JPanel{
     public static BlackBoard blackboard;
-    public static void main(String [] args)
-    {
-        BlackBoard main = new BlackBoard();
-        main.setSize(600,400);
-        main.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        main.setVisible(true);
-    }
 
     /**
      * Constructor for BlackBoard.
      */
     public BlackBoard() {
+        setBackground(Color.PINK);
 
-        JPanel central = new JPanel();
-        central.setBackground(Color.BLUE);
-        GridLayout grid = new GridLayout(7,1);
-        central.setLayout(grid);
-        BorderLayout layout = new BorderLayout();
-        add(central, BorderLayout.CENTER);
-    }
-
-    /**
-     * Implemented Method from ActionListener class that allows the program to
-     * acces the button that has been clicked to invoke an action.
-     * 
-     * @param e
-     */
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String actionCommand = e.getActionCommand();
     }
 
 }
