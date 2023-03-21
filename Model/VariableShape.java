ba933a798382dc79cbaf68320da56af94ff283d9
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-03-21 16:35:45 -0700

    Create VariableShape.java

---
+++ Model/VariableShape.java
@@ -0,0 +1,29 @@
+package View;
+
+import java.awt.*;
+
+
+
+public class VariableShape extends Shape {
+
+
+    public VariableShape(int x, int y){
+        super(x, y);
+    }
+
+    @Override
+    public void draw(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillRect(x, y, 100, 50);
+        g.setColor(Color.black);
+        g.drawRect(x, y, 100, 50);
+        g.drawLine(x, y+10, x+100,y+10);
+        g.drawLine(x+10, y, x+10,y+50);
+    }
+
+    @Override
+    public boolean contains(int x2, int y2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+}
