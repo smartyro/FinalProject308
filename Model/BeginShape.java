ece85706f8e9d7ed3920a67e6333b8cecd1ee785
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-03-21 16:41:26 -0700

    Create BeginShape.java

---
+++ Model/BeginShape.java
@@ -0,0 +1,27 @@
+package Model;
+
+import java.awt.*;
+
+
+
+public class BeginShape extends Shape {
+
+
+    public BeginShape(int x, int y){
+        super(x, y);
+    }
+
+    @Override
+    public void draw(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillOval(x, y, 30, 30);
+        g.setColor(Color.black);
+        g.drawOval(x, y, 30, 30);
+    }
+
+    @Override
+    public boolean contains(int x2, int y2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+}
