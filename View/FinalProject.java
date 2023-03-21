7eb8d6029d533adc0090ca50b5b10cfa89a97c45
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2023-03-20 18:11:03 -0700

    Update FinalProject.java

--- View/FinalProject.java
+++ View/FinalProject.java
@@ -19,6 +19,7 @@ public class FinalProject extends JFrame {
 
 		Blackboard board = Blackboard.getInstance();
 		board.addMouseListener(ControlHandler.getInstance());
+        StatusBar statusBar = StatusBar.getInstance();
 
 		Repository.getRepository().addObserver(board);
 
@@ -26,6 +27,7 @@ public class FinalProject extends JFrame {
         setLayout(layout);
         add(menu, BorderLayout.NORTH);
         add(board, BorderLayout.CENTER);
+        add(statusBar, BorderLayout.SOUTH);
     }
 }
 
