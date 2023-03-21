937b5ffb3643c42fedcfeccf0abc5ca3d647fdb9
GavMeist3r <gstoddard5754@gmail.com>
2023-03-20 18:22:37 -0700

    Added arrow draw method

--- Model/Arrow.java
+++ Model/Arrow.java
@@ -16,8 +16,26 @@ public class Arrow {
 
     }
 
+
     public void draw(Graphics g){
+        int width = 10;
+        int height = 10;
+        int dx = x2 - x1, dy = y2 - y1;
+        double hypotenuse = Math.sqrt(dx*dx + dy*dy);
+        double xDiff = hypotenuse - width, xn = xDiff, yDiff = height, yn = -height, x;
+        double sin = dy / hypotenuse, cos = dx / hypotenuse;
+
+        x = xDiff*cos - yDiff*sin + x1;
+        yDiff = xDiff*sin + yDiff*cos + y1;
+        xDiff = x;
+        x = xn*cos - yn*sin + x1;
+        yn = xn*sin + yn*cos + y1;
+        xn = x;
+
+        int[] xList = {x2, (int) xDiff, (int) xn};
+        int[] yList = {y2, (int) yDiff, (int) yn};
         g.drawLine(x1, y1, x2, y2);
+        g.fillPolygon(xList, yList, 3);
     }
 
 
