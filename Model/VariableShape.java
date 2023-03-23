b330b131e3c2187eda6b3b03ee6a94d7677344dc
keilamohan <60747314+keilamohan@users.noreply.github.com>
2023-03-22 18:29:09 -0700

    Update VariableShape.java

--- Model/VariableShape.java
+++ Model/VariableShape.java
@@ -1,7 +1,7 @@
 package Model;
 
 import java.awt.*;
-
+import static java.lang.Math.sqrt;
 public class VariableShape extends Shape {
 
     private static final int WIDTH = 100;
@@ -31,5 +31,35 @@ public class VariableShape extends Shape {
     public boolean contains(int x2, int y2) {
         return x2 >= getX() && x2 <= getX() + WIDTH &&
                 y2 >= getY() && y2 <= getY() + HEIGHT;
+    }
+    public int[] getArrowPoint(Shape s) {
+        //top point of shape
+        double dist = sqrt(((s.getY() - this.getY()) * (s.getY() - this.getY())) + 
+        (((s.getX() - (this.getX()+WIDTH/2))) * ((s.getX()) - (this.getX()+WIDTH/2))));
+        int[] ret = {this.getX()+WIDTH/2, this.getY()};
+        // bottom of shape
+        double dist2 = sqrt((s.getY() - this.getY()+HEIGHT) * (s.getY() - this.getY()+HEIGHT) + 
+        ((s.getX() - (this.getX()+WIDTH/2)) * ((s.getX()) - (this.getX()+WIDTH/2))));
+        // // left of shape
+        double dist3 = sqrt((s.getY() - (this.getY()+HEIGHT/2)) * (s.getY() - (this.getY()+HEIGHT/2)) + 
+        ((s.getX()) - (this.getX())) * ((s.getX()) - (this.getX())));
+        // // right of circle
+        double dist4 = sqrt((s.getY() - (this.getY()+HEIGHT/2)) * (s.getY() - (this.getY()+HEIGHT/2)) + 
+        ((s.getX()) - (this.getX()+WIDTH)) * ((s.getX()) - (this.getX()+WIDTH)));
+
+        if (dist < dist2){
+            dist = dist2;
+            ret[1] = this.getY()+HEIGHT;
+        }
+        else if (dist > dist3){
+            dist = dist3;
+            ret[0] = this.getX();
+            ret[1] = this.getY()+HEIGHT/2;
+        }
+        else if (dist > dist4){
+            ret[0] = this.getX()+WIDTH;
+            ret[1] = this.getY()+HEIGHT/2;
+        }
+        return ret;
     }
 }
