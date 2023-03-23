f78aa32215e719db3a932273a05a79a9c6d6c9ad
sophie :-) <48779014+smartyro@users.noreply.github.com>
2023-03-22 18:15:36 -0700

    Update InputOutputShape.java

--- Model/InputOutputShape.java
+++ Model/InputOutputShape.java
@@ -6,45 +6,61 @@ import java.awt.Polygon;
 
 public class InputOutputShape extends Shape{
     private static final int numPoints = 4;
-    private static final int length = 100;
-    private static final int height = 50;
-    private static final int diffX = 15;
-    private final int[] xArray = {0, 0, 0, 0};
-    private final int[] yArray = {0, 0, 0, 0};
+    private static int length = 100;
+    private static int height = 50;
+    private int x1, x2, x3, x4;
+    private int y1, y2, y3, y4;
  
 
     public InputOutputShape(int x, int y, String label){
         super(x, y, label);
-        maxIn = 1;
-        maxOut = 1;
     }
 
     @Override
     public void draw(Graphics g) {
-        xArray[0] = getX();
-        xArray[1] = getX() + length;
-        xArray[2] = getX() + length - diffX;
-        xArray[3] = getX() - diffX;
+        x1 = getX();
+        x2 = (getX() + length);
+        x4 = x1 + getDistance(x1, height, length);
+        x3 = x2 - getDistance(x2, height, length);
 
-        yArray[0] = getY();
-        yArray[1] = getY();
-        yArray[2] = getY() + height;
-        yArray[3] = getY() + height;
-        
+        y1 = y2 = getY();
+        y3 = y4 = (getY()+height);
+
+        int[] xArray = {x1, x4, x2, x3};
+        int[] yArray = {y1, y2, y3, y4};
+
         Polygon p = new Polygon(xArray, yArray, numPoints);
+
         g.setColor(Color.WHITE);
         g.fillPolygon(p);
-        g.setColor(Color.BLACK);
+        g.setColor(Color.black);
         g.drawPolygon(p);
     }
 
     @Override
-    public boolean contains(int x2, int y2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    public boolean contains(int x, int y) {
+        int triDistance = (Math.abs(length - x1));
+        // check if inside the square
+        if ((x >= this.x1) && (x <= this.x4) && (y >= this.y1) && (y <= this.y4)){
+            return true;
+        }
+        // check if inside the left triangle
+        else if ((x >= this.x3) && (x<= this.x1) && (x >= (getDistance(x, triDistance, length)) && (y >= this.y1) && (y <= this.y4))){
+            return true;
+        }
+        // check if inside the right triangle
+        else if ((x <= this.x2) && (x<= this.x4) && (x >= (getDistance(x, triDistance, length)) + this.x4) && (y >= this.y1) && (y <= this.y4)){
+            return true;
+        }
+        return false;
     }
 
-    public int getDistance(int x1, int height, int length){
+    @Override
+    public void drawLabel(Graphics g){
+        g.drawString(this.getLabel(), this.getX() + 50, this.getY() + 25);
+    }
+
+    public int getDistance(int x, int height, int length){
         int xdist;
         double a2, b2;
 
