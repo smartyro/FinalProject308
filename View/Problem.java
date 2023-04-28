efa6fb9a57f7aad8e3af4b5d7ad680750a8d364f
keilamohan <60747314+keilamohan@users.noreply.github.com>
2023-04-27 20:11:45 -0700

    Update Problem.java

---
+++ View/Problem.java
@@ -0,0 +1,27 @@
+package View;
+import java.util.ArrayList;
+
+import Model.Shape;
+
+public class Problem {
+    private String probStatement;
+    private ArrayList<Shape> shapes;
+    public Problem(String pstatement, ArrayList<Shape> shapes){
+        probStatement = pstatement;
+        this.shapes = shapes;
+    }   
+
+    public String getProblemStatement()
+    {
+        return probStatement;
+    }
+
+    public ArrayList<Shape> getShapes()
+    {
+        return shapes;
+    }
+    
+
+
+    
+}
