10ddeddba7043d57c6c30ba0f57f608c51c6c7cf
keilamohan <60747314+keilamohan@users.noreply.github.com>
2023-04-28 12:58:27 -0700

    Update Problem.java

--- View/Problem.java
+++ View/Problem.java
@@ -1,15 +1,27 @@
 package View;
 import java.util.ArrayList;
 
+import Model.BeginShape;
+import Model.ConditionShape;
+import Model.EndShape;
+import Model.InputOutputShape;
+import Model.MethodShape;
 import Model.Shape;
+import Model.VariableShape;
 
 public class Problem {
     private static String probStatement;
-    private ArrayList<Shape> shapes;
+    private ArrayList<Shape> shapes = new ArrayList<Shape>();
 
     public Problem(String pstatement, ArrayList<Shape> shapes2){
         probStatement = pstatement;
-        this.shapes = shapes2;
+        this.shapes.add(new BeginShape(0, 0, ""));
+        this.shapes.add(new VariableShape(0, 0, "x = 4"));
+        this.shapes.add(new VariableShape(0, 0, "y = 5"));
+        this.shapes.add(new ConditionShape(0, 0, "if (y > x)"));
+        this.shapes.add(new MethodShape(0, 0, "add_nums(x, y)"));
+        this.shapes.add(new EndShape(0, 0, ""));
+
     }
 
     public static String getProblemStatement()
