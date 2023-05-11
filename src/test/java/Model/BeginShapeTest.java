8936948d11fe4576cd9482e11b28ee347e00611c
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-05-11 15:19:02 -0700

    Add files via upload

---
+++ src/test/java/Model/BeginShapeTest.java
@@ -0,0 +1,24 @@
+package Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeginShapeTest {
+    BeginShape testCase = new BeginShape(100, 100, "testShape");
+
+    @Test
+    void contains_test1() {
+        assertTrue(testCase.contains(120, 120));
+    }
+
+    @Test
+    void contains_test2() {
+        assertTrue(testCase.contains(150, 150));
+    }
+
+    @Test
+    void contains_test3() {
+        assertFalse(testCase.contains(50, 150));
+    }
+}
