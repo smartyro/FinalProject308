2617abf6094a5faa6a8d37ebcff016b4d46efe91
sophie :-) <48779014+smartyro@users.noreply.github.com>
2023-05-12 09:55:57 -0700

    update tutorial for next sprint

--- src/main/java/View/Tutorial.java
+++ src/main/java/View/Tutorial.java
@@ -9,23 +9,26 @@ import java.awt.*;
 
 public class Tutorial extends JInternalFrame{
     public Tutorial(){
-		setSize(300, 200);
-		setVisible(true);
-		setClosable(true);
-		setMaximizable(true);
-		setResizable(true);
-
+		createInternalFrame();
 		JPanel tPanel = new JPanel(new GridLayout(2, 1));
 		JLabel welcome = new JLabel("Welcome to the Functional Flow Block Diagram Tutorial! Here is an example of a proper diagram for the following program: ");
-		welcome.setFont(new Font("Serif", Font.BOLD, 20));
+		welcome.setFont(new Font("Inter", Font.PLAIN, 20));
 		JLabel prompt = new JLabel("Find the greatest common factor of two numbers" + 
 		" and tell the user if it is even or odd.");
-		prompt.setFont(new Font("Serif", Font.PLAIN, 20));
+		prompt.setFont(new Font("Inter", Font.PLAIN, 20));
 		tPanel.add(welcome, BorderLayout.CENTER);
 		tPanel.add(prompt, BorderLayout.CENTER);
 		JPanel bPanel = new JPanel(new GridLayout(1, 1));
-		bPanel.add(new JLabel(new ImageIcon("View/flowchartImg1.png")), BorderLayout.CENTER);
 		add(tPanel, BorderLayout.NORTH);
 		add(bPanel, BorderLayout.CENTER);
     }
+
+	private void createInternalFrame(){
+	setClosable(true);
+        setResizable(true);
+        setMaximizable(true);
+        setIconifiable(true);
+        setSize(600, 400);
+        setVisible(false);
+	}
 }
