bd8f1998ad3cae7f8a03f527a7a3a971881ccaf3
sophie :-) <48779014+smartyro@users.noreply.github.com>
2023-06-14 11:22:43 -0700

    updated helpMenu to include chatGPT integration

--- src/main/java/View/helpMenu.java
+++ src/main/java/View/helpMenu.java
@@ -4,12 +4,16 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class helpMenu extends JMenu implements ActionListener{
+public class helpMenu extends JMenu implements ActionListener {
     JMenuItem docItem;
-    JMenuItem tutItem; 
+    JMenuItem tutItem;
+    JMenuItem chatItem;
+    Chat chat;
+    private int remainingQs;
 
-    public helpMenu(String name){
+    public helpMenu(String name) {
         setText(name);
+        remainingQs = 3;
 
         docItem = new JMenuItem("Documentation");
         docItem.addActionListener(this);
@@ -17,31 +21,63 @@ public class helpMenu extends JMenu implements ActionListener{
         tutItem = new JMenuItem("Tutorial");
         tutItem.addActionListener(this);
 
+        chatItem = new JMenuItem("Ask ChatGPT");
+        chatItem.addActionListener(this);
+
         add(docItem);
         add(tutItem);
+        add(chatItem);
+
+        chat = new Chat("Default input");
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == tutItem){
+        if (e.getSource() == tutItem) {
             createTutorial();
-        }
-        if (e.getSource() == docItem){
-            String message = "The authors of this program are Liam Hyde, Keila Mohan, Jared Swislow, Anna Rosenberg, Gavid Stoddard, and Sophie Martyrossian.\n This application was developed for Cal Poly's CSC 309 Software Engineering II class, taught by Professor Gonzalez-Sanchez, Spring Quarter 2023. \n";
+        } else if (e.getSource() == docItem) {
+            String message = "This is a test!";
             JOptionPane.showConfirmDialog(null, message, "Documentation", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE);
+        } else if (e.getSource() == chatItem) {
+            String response = null;
+            if (remainingQs > 0) {
+                String input = JOptionPane.showInputDialog(null,
+                        "Ask me a question! You have " + remainingQs + " questions remaining.", null,
+                        JOptionPane.PLAIN_MESSAGE);
+
+                try {
+                    response = chat.getResponse(input);
+                } catch (Exception ex) {
+                    JOptionPane.showConfirmDialog(null, "An error occurred while communicating with ChatGPT.", "ERROR!",
+                            JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
+                    ex.printStackTrace();
+                }                
+
+                JOptionPane.showMessageDialog(null,
+                        "Your input is '" + input + "'!\n" + "ChatGPT's Response is: " + response, "ChatGPT's Response",
+                        JOptionPane.PLAIN_MESSAGE);
+                --remainingQs;
+            } else {
+                JOptionPane.showConfirmDialog(null, "You are all out of questions. Please try again.", "ERROR!",
+                        JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 
-    public void createTutorial(){
-        String message = "Welcome to the Functional Flow Block Diagram Tutorial!\n" + 
-        "The shapes for the program is as follows: \n" + 
-        "Begin Shape and End Shape is always a circle. Every program will have a start and end shape. \n" + 
-        "Condition Shape is a diamond. This is used when there is a condition to be met, such as if a value is greater or less than another\n" +
-        "Method Shape is a rectangle with vertical lines. This is used when you are declaring a method, such as 'void message()'\n" +
-        "Instruction Shape is a rectangle without vertical lines. This is used when you are performing an instruction, such as add.\n" + 
-        "Input or Output is a parallelogram. This is used when you are printing, or receiving user input.\n" + 
-        "Variable Declaration is a rectangle with a horizontal and vertical line. This is used for declaring a variable, such as 'int x = 5;'\n" +
-        "Good luck!";
-        JOptionPane.showConfirmDialog(null, message, "Tutorial", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE); 
+    public void createTutorial() {
+        String message = "Welcome to the Functional Flow Block Diagram Tutorial!\n\n" +
+                "The shapes for the program is as follows: \n" +
+                "Begin Shape and End Shape is always a CIRCLE. Every program will have a start and end shape. \n" +
+                "Condition Shape is a DIAMOND. This is used when there is a condition to be met, such as if a value is greater or less than another\n"
+                +
+                "Method Shape is a RECTANGLE WITH VERTICAL LINES. This is used when you are declaring a method, such as 'void message()'\n"
+                +
+                "Instruction Shape is a PLAIN RECTANGLE. This is used when you are performing an instruction, such as add.\n"
+                +
+                "Input or Output is a PARALLELOGRAM. This is used when you are printing, or receiving user input.\n" +
+                "Variable Declaration is a RECTANGLE WITH A HORIZONTAL AND VERTICAL LINE. This is used for declaring a variable, such as 'int x = 5;'\n"
+                +
+                "\nGood luck!";
+        JOptionPane.showConfirmDialog(null, message, "Tutorial", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE);
     }
 }
